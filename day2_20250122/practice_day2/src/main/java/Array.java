dad40f38157256f70b7c74e9c8dffcea9c5b01db
yoko-Developer <whitesnow.wave@gmail.com>
2025-01-24 00:35:54 +0900

    コード修正

---
+++ day2_20250122/practice_day2/src/main/java/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+
+        System.out.println(0);
+    }
+}
---
+++ day2_20250122/practice_day2/src/main/java/ArrayFor.java
@@ -0,0 +1,39 @@
+public class ArrayFor {
+    public static void main(String[] args) {
+        int[] array = new int[3];
+
+        int sum = 0;
+
+        // 配列の初期化
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+
+        // 普通のfor文
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("array[" + i + "]: " + array[i]);
+            sum += array[i];
+        }
+
+        // 平均値
+        double average = sum / (double) array.length;
+        System.out.println("合計: " + sum);
+        System.out.println("平均値: " + average);
+
+        // 拡張for文
+        System.out.println("-------------------");
+        System.out.println("拡張for文での実装");
+        sum = 0;
+        for (int num : array) {
+
+            System.out.println("配列の要素: " + num);
+            // 合計値
+            sum += num;
+        }
+        // 平均値
+        average = sum / (double) array.length;
+
+        System.out.println("合計: " + sum);
+        System.out.println("平均値: " + average);
+    }
+}
