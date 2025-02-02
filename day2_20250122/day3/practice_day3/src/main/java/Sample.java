18342b1f1446b389d230da838ac8a7696622dfdd
yoko-Developer <whitesnow.wave@gmail.com>
2025-02-02 21:47:34 +0900

    掛け算と配列のコード作成

---
+++ day2_20250122/day3/practice_day3/src/main/java/Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+    public static void test(int a) {
+        System.out.println("test:" + 10);
+    }
+
+    // argsではなくてもOK
+    public static void main(String[] args) {
+        test(10);
+    }
+}
