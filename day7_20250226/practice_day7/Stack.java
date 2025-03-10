67f4f2791acf3d64cf461f2e4996d49e8e59d58f
yoko-Developer <whitesnow.wave@gmail.com>
2025-03-10 23:59:42 +0900

    コンストラクタ

---
+++ day7_20250226/practice_day7/Stack.java
@@ -0,0 +1,18 @@
+public class Stack {
+
+    // 0で初期化すると０から入れられる。どこまで入れられたか確認する
+    private int index = 0;
+    private String[] values = new String[10];
+
+    public void push(String value) {
+        this.values[this.index] = value;
+        // ++だとnullを返すので--だったらCBAと出力する
+        this.index++;
+    }
+
+    public String pop() {
+        // --なのでCBAと出力する
+        this.index--;
+        return this.values[this.index];
+    }
+}
