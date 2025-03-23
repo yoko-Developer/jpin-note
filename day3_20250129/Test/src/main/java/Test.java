1b9cf08030f7621f53becaa87577dcfc556a08c0
yoko-Developer <whitesnow.wave@gmail.com>
2025-03-23 23:32:30 +0900

    Remove practice directory

--- day3_20250129/Test/src/main/java/Test.java
+++ day3_20250129/Test/src/main/java/Test.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Test {
+public class Test {
+    public static void main(String[] args) {
+        String str1 = "Java";               // 文字列プール
+        String str2 = new String("Java");   // ヒープメモリ
+        String str3 = "Java";               // 文字列プール（str1と同じ）
+
+        System.out.println((str1 == str2) + " " + (str1 == str3));
+    }
 }
