42239676861348ad986e65600fb620a580c4537f
yoko-Developer <whitesnow.wave@gmail.com>
2025-03-23 23:34:17 +0900

    Remove Test directory from tracking

--- day3_20250129/Test/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String str1 = "Java";               // 文字列プール
-        String str2 = new String("Java");   // ヒープメモリ
-        String str3 = "Java";               // 文字列プール（str1と同じ）
-
-        System.out.println((str1 == str2) + " " + (str1 == str3));
-    }
-}
