6f50d3df96a61b408e89592b376e0498d85e3dc9
yoko-Developer <whitesnow.wave@gmail.com>
2025-07-29 22:06:38 +0900

    2回目メモ

---
+++ jpinPractice/20250723/src/main/java/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30};
+
+        for (int n : array) {
+            System.out.println(n);
+        }
+    }
+}
---
+++ jpinPractice/20250723/src/main/java/Loop.java
@@ -0,0 +1,9 @@
+public class Loop {
+    public static void main(String[] args) {
+
+        // *で四角を作る
+        for (int j = 0; j < 3; j++) {
+            System.out.println("***");
+        }
+    }
+}
