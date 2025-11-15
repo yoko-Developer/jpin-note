5df8948ce979bce0d21d1a907754d0f4301ee662
yoko-Developer <whitesnow.wave@gmail.com>
2025-11-16 05:19:42 +0900

    テスト実装

--- jpinPractice/src/main/java/date20250917/Sample2.java
+++ jpinPractice/src/main/java/date20250917/Sample2.java
@@ -10,12 +10,12 @@ public class Sample2 {
 
         ShippingAddress address =
                 builder
-                        .postalCode("123-456")
-                        .prefecture(Prefecture.TOKYO)
-                        .city("Shinjuku-ku")
-                        .address("XXX")
-                        .phoneNumber("03-1234-5678")
-                        .build();
+                        .postalCode("123-456") // build型を返している
+                        .prefecture(Prefecture.TOKYO) // build型を返している
+                        .city("Shinjuku-ku") // build型を返している
+                        .address("XXX") // build型を返している
+                        .phoneNumber("03-1234-5678") // build型を返している
+                        .build(); // ここでようやくShippingAddress型を返す
 
     }
 }
