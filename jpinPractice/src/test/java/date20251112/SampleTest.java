f2627dfc82651546525f2c94c3cbd73514e8ecf1
yoko-Developer <whitesnow.wave@gmail.com>
2025-11-22 18:59:26 +0900

    JUNIT構築

--- jpinPractice/src/test/java/date20251112/SampleTest.java
+++ jpinPractice/src/test/java/date20251112/SampleTest.java
@@ -1,4 +1,23 @@
+package date20251112;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class SampleTest {
-  
-}
+
+public class SampleTest {
+
+        @Test
+        public void testPlus() {
+            Sample s = new Sample();
+            int result = s.plus(2, 3);
+            assertEquals(5, result); // 期待値, 結果
+        }
+
+        @Test
+        public void testMinus() {
+            Sample s = new Sample();
+            int result = s.minus(5, 3);
+            assertEquals(2, result);
+        }
+
+    }
